895a56b56826c7e18e76c6e0749eb0a7e9d35c93
Leona Dayang <leyowwnadayang@gmail.com>
2023-10-10 23:16:16 +0800

    Modify fragment_profile.xml and MainActivity.java

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -6,8 +6,11 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.capstone.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,16 +21,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
+        //Navigation if-else
         binding.bottomNavigationView.setOnItemReselectedListener(item -> {
 
             int itemId = item.getItemId();
+
             if (itemId == R.id.home) {
                 replaceFragment(new HomeFragment());
             } else if (itemId == R.id.dine) {
                 replaceFragment(new DineFragment());
-            } else if (itemId == R.id.floatingBook) {
-                replaceFragment(new BookFragment());
             } else if (itemId == R.id.products) {
                 replaceFragment(new ProductsFragment());
             } else if (itemId == R.id.profile) {
@@ -35,8 +39,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Unclickable menu item
+        NavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        Menu bottom_nav_menu = bottomNavigationView.getMenu();
+        MenuItem itemToMakeUnclickable = bottom_nav_menu.findItem(R.id.unclickableBooking);
+        itemToMakeUnclickable.setEnabled(false);
+
     }
 
+    //Fragments for Navigation
     private void replaceFragment(Fragment fragment) {
 
         FragmentManager fragmentManager = getSupportFragmentManager();

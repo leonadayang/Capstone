55c57161a598d974d0fb84376d57d5aaa7b9e3cb
Leona Dayang <leyowwnadayang@gmail.com>
2023-10-20 18:29:20 +0800

    onclick for image button, back button, NotificationsFragment

--- app/src/main/java/com/example/capstone/ActivityHistoryFragment.java
+++ app/src/main/java/com/example/capstone/ActivityHistoryFragment.java
@@ -3,13 +3,17 @@ package com.example.capstone;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ActivityHistoryFragment extends Fragment {
 
+    ImageButton btnBackAH;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,7 +23,21 @@ public class ActivityHistoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activity_history, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_activity_history, container, false);
+
+        //onclick buttons
+        btnBackAH = view.findViewById(R.id.buttonBackAH);
+
+        btnBackAH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fProfile = new ProfileFragment();
+                FragmentTransaction ftProfile = getActivity().getSupportFragmentManager().beginTransaction();
+                ftProfile.replace(R.id.frameLayoutNavigation,fProfile).commit();
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/example/capstone/ProfileFragment.java
+++ app/src/main/java/com/example/capstone/ProfileFragment.java
@@ -9,10 +9,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class ProfileFragment extends Fragment {
 
     Button btnViewAll;
+    ImageButton btnImage1, btnImage2, btnImage3;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,10 +29,38 @@ public class ProfileFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        //onclick of View all button
+        //onclick of buttons
         btnViewAll = view.findViewById(R.id.buttonViewAll);
+        btnImage1 = view.findViewById(R.id.image1);
+        btnImage2 = view.findViewById(R.id.image2);
+        btnImage3 = view.findViewById(R.id.image3);
 
         btnViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fActivityHistory = new ActivityHistoryFragment();
+                FragmentTransaction ftActivityHistory = getActivity().getSupportFragmentManager().beginTransaction();
+                ftActivityHistory.replace(R.id.frameLayoutNavigation,fActivityHistory).commit();
+            }
+        });
+
+        btnImage1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fActivityHistory = new ActivityHistoryFragment();
+                FragmentTransaction ftActivityHistory = getActivity().getSupportFragmentManager().beginTransaction();
+                ftActivityHistory.replace(R.id.frameLayoutNavigation,fActivityHistory).commit();
+            }
+        });
+        btnImage2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fActivityHistory = new ActivityHistoryFragment();
+                FragmentTransaction ftActivityHistory = getActivity().getSupportFragmentManager().beginTransaction();
+                ftActivityHistory.replace(R.id.frameLayoutNavigation,fActivityHistory).commit();
+            }
+        });
+        btnImage3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Fragment fActivityHistory = new ActivityHistoryFragment();

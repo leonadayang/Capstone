38eec9cd813cb39d361113c4cfea95e2e157d648
Leona Dayang <leyowwnadayang@gmail.com>
2023-10-31 12:29:16 +0800

    dialog box for edit profile picture

--- app/src/main/java/com/example/capstone/ProfileFragment.java
+++ app/src/main/java/com/example/capstone/ProfileFragment.java
@@ -1,23 +1,32 @@
 package com.example.capstone;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ProfileFragment extends Fragment {
 
     Button btnViewAll;
     ImageButton btnImage1, btnImage2, btnImage3, btnSettings;
+    FloatingActionButton fabEditProfile;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,12 +40,13 @@ public class ProfileFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        //onclick of buttons
+        //buttons
         btnSettings = view.findViewById(R.id.buttonSettings);
         btnViewAll = view.findViewById(R.id.buttonViewAll);
         btnImage1 = view.findViewById(R.id.image1);
         btnImage2 = view.findViewById(R.id.image2);
         btnImage3 = view.findViewById(R.id.image3);
+        fabEditProfile = view.findViewById(R.id.fabEditProfilePicture);
 
         //for settings button
         btnSettings.setOnClickListener(new View.OnClickListener() {
@@ -82,6 +92,14 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        //for edit profile pic button
+        fabEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(view);
+            }
+        });
+
         return view;
     }
 
@@ -118,5 +136,35 @@ public class ProfileFragment extends Fragment {
 
         popup.inflate(R.menu.settings_popup_menu);
         popup.show();
+    }
+
+    //dialog box for edit profile pic
+    private void showDialog(View view) {
+        final Dialog dialog = new Dialog(requireContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_box_layout);
+
+        LinearLayout takePhoto = dialog.findViewById(R.id.linearTakePhoto);
+        LinearLayout choosePhoto = dialog.findViewById(R.id.linearChoosePhoto);
+
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(requireContext(), "Take a photo is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        choosePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(requireContext(), "Choose from gallery is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
 }

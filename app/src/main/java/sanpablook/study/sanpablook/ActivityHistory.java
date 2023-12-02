cf7a305c23ed557078de10e88c99349401d06fcd
TartPops6 <pikuradezu@gmail.com>
2023-12-02 20:49:57 +0800

    Renamed the java folders into com.study.sanpablook

---
+++ app/src/main/java/sanpablook/study/sanpablook/ActivityHistory.java
@@ -0,0 +1,29 @@
+package sanpablook.study.sanpablook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.study.sanpablook.R;
+
+public class ActivityHistory extends AppCompatActivity {
+
+    ImageButton btnBackAH;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        btnBackAH = findViewById(R.id.buttonBackAH);
+
+        btnBackAH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/sanpablook/study/sanpablook/DineCasaActivity.java
@@ -0,0 +1,64 @@
+package sanpablook.study.sanpablook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.study.sanpablook.R;
+
+public class DineCasaActivity extends AppCompatActivity {
+    ImageButton btnShare, btnBack;
+    Button reserveNowBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dine_casa);
+        //buttons
+        btnBack = findViewById(R.id.btnBack);
+        btnShare = findViewById(R.id.btnShare);
+        reserveNowBtn = findViewById(R.id.reserveNowBtn1);
+
+        //back button
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        //share button
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                String Body = "Share this App";
+                String Sub = "http://casa_location.google.com";
+                intent.putExtra(Intent.EXTRA_TEXT, Body);
+                intent.putExtra(Intent.EXTRA_TEXT, Sub);
+                startActivity(Intent.createChooser(intent, "Share using"));
+            }
+        });
+
+
+        //reserve now button
+//        reserveNowBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent=new Intent(DineCasaActivity.this, com.example.capstone.DineCasaReservationActivity.class);
+//                startActivity(intent);
+//
+//            }
+//        });
+    }
+    //message button
+    public void btnMessageCasa(View v) {
+        String number = "09178126687";
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", number, null)));
+    }
+}
